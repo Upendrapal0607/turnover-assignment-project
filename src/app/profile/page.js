ec0c248f2b682a2e07fd760d835fd5920e865218
Upendra Pal <up06072000@gmail.com>
2024-03-26 11:34:05 +0530

    some changes in profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -8,7 +8,7 @@ import LoginPage from '../components/login'
       localStorage.clear("token")
     }
   return (
-    !tokenData?.token?<LoginPage/>:<div className='w-full h-[82vh] bg-white flex justify-center items-center'>
+    tokenData?.token?<LoginPage/>:<div className='w-full h-[82vh] bg-white flex justify-center items-center'>
      <button
       className="bg-slate-900 hover:bg-slate-950 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
       onClick={LogOuthandle}
